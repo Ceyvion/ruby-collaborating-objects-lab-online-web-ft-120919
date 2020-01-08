97514e34cbdf7d630365fa1f8ff0f02dac12eb66
Ceyvion Biggs <ceyvionb@gmail.com>
2020-01-08 17:07:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,9 +13,12 @@ def self.all
   @@all 
 end 
   
-def self.new_by_filename(filename)
-  name = filename.split(" - ")[1]
-end 
+  def self.new_by_filename(filename)
+    artist,song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+    end
   
   
 def artist_name=(name)
