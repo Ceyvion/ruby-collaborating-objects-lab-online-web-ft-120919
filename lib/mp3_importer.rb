bf507e432e3123ab83790c064d0975be88974899
Ceyvion Biggs <ceyvionb@gmail.com>
2020-01-08 17:09:06 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,26 @@
+class MP3Importer
+  attr_accessor :path 
+  
+
+
+
+
+def initialize(path)
+  @path = path 
+  
+end 
+
+def files
+  songs = []
+  files = Dir.open(@path)
+  files.each {|song| song.include?(".mp3")? songs << songs : nil}
+  songs
+end 
+
+def import
+    files.each {|name| Song.new_by_filename(name)}
+  end
+
+
+
+end 
